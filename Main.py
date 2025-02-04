2cdeddfbbc0b19bff259a804247702df79e0f493
Wantuzpro <homkagut@gmail.com>
2025-02-04 15:13:00 +0200

    Створив класи

--- Main.py
+++ Main.py
@@ -1,18 +1,30 @@
+import random
+
 class Human:
-    height = 170
-    age = 21
+    def __init__(self, name):
+        self.name = name
+        self.money = 100
+        self.happiness = 50
+        self.satiety = 50
+        self.job = None
+        self.car = None
+        self.house = None
 
-class Student(Human):
-    height = 180
-    age = 17
+class Auto:
+    def __init__(self, brand, fuel, durability, fuel_consumption):
+        self.brand = brand
+        self.fuel = fuel
+        self.durability = durability
+        self.fuel_consumption = fuel_consumption
 
-class Worker(Human):
-    pass
+class House:
+    def __init__(self):
+        self.mess = 0
+        self.food = 5
 
-Nick = Student()
-Lion = Worker()
+class Job:
+    def __init__(self, position, salary, happiness_loss):
+        self.position = position
+        self.salary = salary
+        self.happiness_loss = happiness_loss
 
-print(Nick.height)
-print(Lion.height)
-print(Nick.age)
-print(Lion.age)
